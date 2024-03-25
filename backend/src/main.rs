51032a038cda17709f8195782d1e4a601eaf4fe9
sneakycrow <zach@sneakycrow.dev>
2024-03-25 15:23:20 -0700

    move manager to todo_manager

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,11 @@
 mod config;
 mod db;
-mod manager;
+mod todo_manager;
 
 pub mod todo {
     tonic::include_proto!("todo");
+    tonic::include_proto!("list");
+    tonic::include_proto!("user");
 }
 
 use config::Config;
@@ -13,7 +15,7 @@ use tonic::transport::Server;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server_config = Config::default();
-    let todo_manager = manager::BackendToDoManager::default();
+    let todo_manager = todo_manager::BackendToDoManager::default();
     println!("ToDoServer listening on {}", server_config.server.addr);
 
     Server::builder()
